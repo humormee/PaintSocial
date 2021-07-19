420f0581ab6d8e168d58b4a17fc46a6e6c6aec3b
Justin Ernst <jernst766@gmail.com>
2021-07-19 18:50:27 -0500

    getting more useful errors

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,9 +1,6 @@
 import { combineReducers } from 'redux';
 import session from './session_api_reducer';
-<<<<<<< HEAD
-=======
 import errors from './errors_reducer';
->>>>>>> Auth-Frontend
 
 const RootReducer = combineReducers({
   session,
