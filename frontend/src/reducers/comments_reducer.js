181c65e185f0c89d9ba461261d7dded8cd4f5063
Justin Ernst <jernst766@gmail.com>
2021-08-03 13:48:05 -0500

    add comments reducer

---
+++ frontend/src/reducers/comments_reducer.js
@@ -0,0 +1,29 @@
+import {
+  RECEIVE_PAINTING_COMMENTS,
+  RECEIVE_COMMENT,
+  RECEIVE_NEW_COMMENT,
+  REMOVE_COMMENT
+} from "../actions/comment_actions";
+
+const commentsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  let nextState = Object.assign({}, oldState);
+  switch (action.type) {
+    case RECEIVE_PAINTING_COMMENTS:
+      nextState.paintingComments = action.comments.data;
+      return nextState;
+    case RECEIVE_COMMENT:
+      nextState.comment = action.comment.data;
+      return nextState;
+    case RECEIVE_NEW_COMMENT:
+      nextState.next = action.comment.data;
+      return nextState;
+    case REMOVE_COMMENT:
+      delete nextState[action.id];
+      return nextState;
+    default:
+      return oldState;
+  }
+}
+
+export default commentsReducer;
--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import paintingsReducer from './paintings_reducer';
+import commentsReducer from './comments_reducer';
 
 const entitiesReducer = combineReducers ({
   paintings: paintingsReducer,
+  comments: commentsReducer
 })
 
 export default entitiesReducer;
