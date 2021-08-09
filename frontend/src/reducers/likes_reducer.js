4550cc5db3fca7ee782fc128a517512ad5247136
Justin Ernst <jernst766@gmail.com>
2021-08-09 18:27:02 -0500

    add like reducer

--- frontend/src/reducers/likes_reducer.js
+++ frontend/src/reducers/likes_reducer.js
@@ -5,5 +5,21 @@ import {
 } from "../actions/like_actions";
 
 const likesReducer = (oldState = {}, action) => {
-  Objec.freeze(oldState)
+  Object.freeze(oldState);
+  let nextState = Object.assign({}, oldState);
+  switch (action.type) {
+    case RECEIVE_PAINTING_LIKES:
+      nextState.paintingLikes = action.likes.oldStatae;
+      return nextState;
+    case RECEIVE_LIKE:
+      nextState.like = action.like.data;
+      return nextState;
+    case REMOVE_LIKE:
+      delete nextState[action.id.data.deletedLike];
+      return nextState;
+    default:
+      return oldState;
+  }
 }
+
+export default likesReducer;
