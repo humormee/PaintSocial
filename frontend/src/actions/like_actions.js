e56d60c468afb20a3354b9dfc8565c7284ca363c
Justin Ernst <jernst766@gmail.com>
2021-08-09 18:15:16 -0500

    add like actions / utils

---
+++ frontend/src/actions/like_actions.js
@@ -0,0 +1,46 @@
+import {
+  getPaintingLikes,
+  createLike,
+  deleteLike
+} from '../util/like_api_util';
+
+export const RECEIVE_PAINTING_LIKES = 'RECEIVE_PAINTING_LIKES';
+export const REMOVE_LIKE = 'REMOVE_LIKE';
+export const RECEIVE_LIKE = 'RECEIVE_LIKE';
+
+const receivePaintingLikes = likes => ({
+  type: RECEIVE_PAINTING_LIKES,
+  likes
+});
+
+const receiveLike = like => ({
+  type: RECEIVE_LIKE,
+  like
+});
+
+const removeLike = likeId => ({
+  type: REMOVE_LIKE,
+  likeId
+});
+
+export const fetchPaintingLikes = paintingId => dispatch => {
+  getPaintingLikes(paintingId)
+      .then(likes => dispatch(receivePaintingLikes(likes)))
+      .catch(err => console.log(err))
+}
+
+export const fetchPainting = paintingId => dispatch => {
+  return (
+    getPainting(paintingId)
+        .then(painting => dispatch(receivePainting(painting)))
+        .catch(err => console.log(err))
+  )
+}
+
+export const eraseLike = likeId => dispatch => {
+  return (
+    deleteLike(likeId)
+        .then(likeId => dispatch(removeLike(likeId)))
+        .catch(err => console.log(err))
+  )
+}
