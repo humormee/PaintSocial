7c2708cd4f8018547454e0f86a17e912a9ca4444
Eric Tran <eric.tran119@gmail.com>
2021-08-10 16:59:01 -0400

    add paint brush gif on splash

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -8,7 +8,8 @@ class MainPage extends React.Component {
       <div>
         <div className="front-logo">
             <Link to="/">
-                <img src="https://cdn.discordapp.com/attachments/597985513701376013/867240581189337098/PaintSocial_Logo_Bigger.png" alt="PaintSocialLogo"/>
+                {/* <img src="https://cdn.discordapp.com/attachments/597985513701376013/867240581189337098/PaintSocial_Logo_Bigger.png" alt="PaintSocialLogo"/> */}
+                <img src="https://creatr-seed.s3.amazonaws.com/paintsmile-transparent.gif" alt="paintbrushisbroken" />
             </Link>
         </div>
       </div>
